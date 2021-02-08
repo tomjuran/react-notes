5d6a31b444f59493b714981f84406b325e6161a3
tomjuran <68550775+tomjuran@users.noreply.github.com>
2021-02-08 12:29:52 -0500

    Update Input.js

--- src/Input.js
+++ src/Input.js
@@ -7,14 +7,14 @@ class Input extends Component {
         input: '',
         output: ''
       };
-      // Change code below this line
+     
       this.handleChange = this.handleChange.bind(this);
       this.handleChangee = this.handleChangee.bind(this);
       this.reset = this.reset.bind(this);
   
-      // Change code above this line
+      
     }
-    // Change code below this line
+ 
     reset() {
         this.setState({
           input: '',
@@ -31,18 +31,18 @@ class Input extends Component {
       output: event.target.value
     });
   }
-    // Change code above this line
+    
     render() {
       return (
         <div>
-          { /* Change code below this line */}
+         
           <h4>Input</h4>
   <input value={this.state.input} onChange={this.handleChange.bind(this)}/>
   <button className='reset' onClick={this.reset}>Reset</button>
   <h4>Output</h4>
   <input value={this.state.output} onChange={this.handleChangee.bind(this)}/>
   <button className='reset' onClick={this.reset}>Reset</button>
-          { /* Change code above this line */}
+         
           <h4>Controlled Input:</h4>
           <p>{this.state.input}</p>
           <h4>Controlled Output:</h4>
